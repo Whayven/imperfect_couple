87b38d8e47a3bf44bcb3dc015710d21155595255
Wayne <waynefosterjr@hotmail.com>
2023-09-18 12:09:07 -0400

    chore:cleanup unused var/files

--- constants.ts
+++ constants.ts
@@ -7,6 +7,4 @@ export const marriageStatus = [
     { value: 'other', label: 'Other' },
 ];
 export const AVATAR_API = "https://ui-avatars.com/api";
-export const API_URL = "http://172.22.96.1:1337/api";
-export const AUTH_TOKEN = "authToken";
-export const BEARER = "Bearer";
+
